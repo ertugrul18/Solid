a0b807bbc296ea5bd7bfea6c2a28af75bc986aa9
ertugrul18 <quars18@gmail.com>
2022-11-07 19:56:11 +0300

    .

---
+++ DIP/Loglama/Concrete/Loglayici.cs
@@ -0,0 +1,19 @@
+﻿using DIP.Loglama.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIP.Loglama.Concrete
+{
+    public class Loglayici
+    {
+
+        public void Logla(ILogger logger, string message) 
+        { 
+            logger.Logla(message);
+        
+        }
+    }
+}
